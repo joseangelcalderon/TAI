14158a63a943a9452074745055f2fc9c496a7da1
joseangelcalderon <joseangelcalderonvillar@gmail.com>
2023-04-07 12:44:50 +0200

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import { mock1 } from './mock';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [data,setData]=useState(mock1);
+  const [pregunta,setPregunta]= useState({});
+  const [dif,setDif]=useState(0);
+  const [auxiliar, setAuxiliar]=useState(false);
+  const [respuesta, setRespuesta]=useState("");
+
+
+   function genera_pregunta(dif){
+      const aux= data.preguntas.filter(preg => preg.dificultad.includes(dif))
+      console.log(aux)
+      setPregunta(aux)
+      setAuxiliar(true);  
+}
+
+
+  function comprueba_respuesta(texto){
+    //const aux= props.theproducts.filter(prod => prod.title.toLowerCase().includes(filtro) );
+
+    if(texto == pregunta[0].respuesta_correcta){
+        setRespuesta("Correcta")
+        console.log("Acierto")
+        var aux=dif +1;
+        console.log(aux)
+        setDif(aux)
+        genera_pregunta(aux)
+    }else{
+        setRespuesta("Incorrecta")
+        console.log("Fallo")
+        var aux = dif -1;
+        setDif(aux)
+        genera_pregunta(aux)
+    }
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <p>Test Adaptativo Informatizado</p>
       </header>
+      <button id="generar" onClick={()=>genera_pregunta(dif)}>Genera Pregunta</button>
+      <p>La dificultad de la pregunta es: {auxiliar ? pregunta[0].dificultad : "No se ha generado pregunta"} </p>
+
+      <h1> {auxiliar ? pregunta[0].pregunta : "No se ha generado pregunta"} </h1>
+      
+      <button id="preguntas" onClick={()=>comprueba_respuesta(pregunta[0].respuesta_correcta)}>{auxiliar ? pregunta[0].respuesta_correcta : "No se ha generado pregunta"}</button>
+      <button id="preguntas" onClick={()=>comprueba_respuesta(pregunta[0].respuesta_incorrecta1)}>{auxiliar ? pregunta[0].respuesta_incorrecta1 : "No se ha generado pregunta"}</button>
+      <button id="preguntas" onClick={()=>comprueba_respuesta(pregunta[0].respuesta_incorrecta2)}>{auxiliar ? pregunta[0].respuesta_incorrecta2 : "No se ha generado pregunta"}</button>
+      <button id="preguntas" onClick={()=>comprueba_respuesta(pregunta[0].respuesta_incorrecta3)}>{auxiliar ? pregunta[0].respuesta_incorrecta3 : "No se ha generado pregunta"}</button>
+    
+      <p>La respuesta es: {auxiliar ? respuesta : "No se ha generado pregunta"}</p>
+      
     </div>
   );
 }
 
 export default App;
+//07-4-2023: El problema que estoy teniendo es que el valor de la dificultad que genera una pregunta no se actualiza, cada vez que pulsas al botón correcto o incorrecto, sino que 
+//se vuelve a generar una pregunta cada vez que llamas al botón. En cambio, si pulsas correcta o incorrectamente, y le das a generar pregunta, entonces
+//si que saldría la pregunta que debería.
+
+//Solución: En vez de pasarle el hook de dif, le pasamos la variable aux y ya funciona como debería. Esto soluciona los problemas y no habría ningún problema a la hora de sincronizar la dificultad con la variable auxiliar.
---
+++ src/mock.js
@@ -0,0 +1,113 @@
+export const mock1 = {
+    "preguntas": [
+        {
+            "id":"0",
+            "dificultad": "0",
+            "pregunta": "Hola que tal",
+            "respuesta_correcta":"Bien",
+            "respuesta_incorrecta1":"Mal",
+            "respuesta_incorrecta2":"Fatal",
+            "respuesta_incorrecta3":"Horrible"
+        },
+        
+        {
+            "id":"1",
+            "dificultad": "1",
+            "pregunta": "Por que estas mal",
+            "respuesta_correcta": "No se",
+            "respuesta_incorrecta1": "Porque si",
+            "respuesta_incorrecta2": "Porque no",
+            "respuesta_incorrecta3": "Porque xd"   
+        },
+        
+        {
+            "id":"2",
+            "dificultad": "2",
+            "pregunta":"Ah bueno dimelo",
+            "respuesta_correcta": "Vale ahora te lo digo",
+            "respuesta_incorrecta1":"No",
+            "respuesta_incorrecta2":"No tio",
+            "respuesta_incorrecta3":"No tronco"  
+        },
+
+        {
+            "id":"3",
+            "dificultad": "3",
+            "pregunta":"Pregunta3?",
+            "respuesta_correcta":"Si",
+            "respuesta_incorrecta1":"No",
+            "respuesta_incorrecta2":"No",
+            "respuesta_incorrecta3":"No"
+        },
+
+        {
+            "id":"4",
+            "dificultad":"4",
+            "pregunta":"Pregunta4?",
+            "respuesta_correcta":"Si",
+            "respuesta_incorrecta1":"No",
+            "respuesta_incorrecta2":"No",
+            "respuesta_incorrecta3":"No"
+        },
+
+        {
+            "id":"5",
+            "dificultad":"5",
+            "pregunta":"Pregunta5?",
+            "respuesta_correcta":"Si",
+            "respuesta_incorrecta1":"No",
+            "respuesta_incorrecta2":"No",
+            "respuesta_incorrecta3":"No"
+        },
+
+        {
+            "id":"6",
+            "dificultad":"6",
+            "pregunta":"Pregunta6?",
+            "respuesta_correcta":"Si",
+            "respuesta_incorrecta1":"No",
+            "respuesta_incorrecta2":"No",
+            "respuesta_incorrecta3":"No"
+        },
+
+        {
+            "id":"7",
+            "dificultad":"7",
+            "pregunta":"Pregunta7?",
+            "respuesta_correcta":"Si",
+            "respuesta_incorrecta1":"No",
+            "respuesta_incorrecta2":"No",
+            "respuesta_incorrecta3":"No"
+        },
+
+        {
+            "id":"8",
+            "dificultad":"8",
+            "pregunta":"Pregunta8?",
+            "respuesta_correcta":"Si",
+            "respuesta_incorrecta1":"No",
+            "respuesta_incorrecta2":"No",
+            "respuesta_incorrecta3":"No"
+        },
+
+        {
+            "id":"9",
+            "dificultad":"9",
+            "pregunta":"Pregunta9?",
+            "respuesta_correcta":"Si",
+            "respuesta_incorrecta1":"No",
+            "respuesta_incorrecta2":"No",
+            "respuesta_incorrecta3":"No"
+        },
+
+        {
+            "id":"10",
+            "dificultad":"10",
+            "pregunta":"Pregunta10?",
+            "respuesta_correcta":"Si",
+            "respuesta_incorrecta1":"No",
+            "respuesta_incorrecta2":"No",
+            "respuesta_incorrecta3":"No"
+        }
+    ]
+} 
